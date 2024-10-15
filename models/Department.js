1bab5f663dee9a923b1d63ae44ab7db5a413fe71
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-10-15 12:56:56 +0530

    add

---
+++ models/Department.js
@@ -0,0 +1,24 @@
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const Department = sequelize.define('Department', {
+    name: {
+        type: DataTypes.STRING,
+        allowNull: false
+    },
+    location: {
+        type: DataTypes.STRING,
+        allowNull: false
+    }
+}, {
+    tableName: 'departments',
+    timestamps: false
+});
+
+Department.sync().then(() => {
+    console.log('Department table has been created (if it didn\'t exist).');
+}
+);
+
+module.exports = Department;
---
+++ models/EmployeeRole.js
@@ -0,0 +1,18 @@
+
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+
+const EmployeeRole = sequelize.define('EmployeeRole', {
+    employee_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    role_id: {
+        type: DataTypes.INTEGER,
+        allowNull: false
+    }
+}, {
+    tableName: 'employee_roles'
+});
+
+module.exports = EmployeeRole;
