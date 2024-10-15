e458485b025655cca3d3b35104a67ef9a1a0d13f
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-10-15 12:33:02 +0530

    feat(backend) : updated paths

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
 const express = require('express');
-const bodyParser = require('body-parser');
 const dotenv = require('dotenv');
 const db = require('./config/database');
-
+const sequelize = require('./config/database'); // Make sure to import sequelize
 
 dotenv.config();
 
 const app = express();
-app.use(bodyParser.json());
-
+app.use(express.json()); 
 
 const employeeRoutes = require('./routes/employeeRoutes');
-// const departmentRoutes = require('./routes/employeeRoutes');
-// const roleRoutes = require('./routes/roleRoutes');
+const departmentRoutes = require('./routes/departmentsRoutes');
+const roleRoutes = require('./routes/rolesRoutes');
+const employeeRolesRoutes = require('./routes/employeeRoleRoutes');
 
 
 app.use('/employees', employeeRoutes);
-// app.use('/departments', departmentRoutes);
-// app.use('/roles', roleRoutes);
+app.use('/departments', departmentRoutes);
+app.use('/roles', roleRoutes);
+app.use('/employee-roles', employeeRolesRoutes);
+
 
 
 // Sync the models with the database
 const syncDatabase = async () => {
   try {
-      await sequelize.sync({ force: true }); // force: true will drop tables if they exist and recreate them
+      await sequelize.sync({ force: false }); // Change to false in production
       console.log('Database & tables created!');
   } catch (error) {
       console.error('Error creating database tables:', error);
   }
 };
-
 syncDatabase(); // Call the function to sync the database
 
 
