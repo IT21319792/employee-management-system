3b9e90781544519d611fdf3a12d24daf9f6ad97b
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-10-21 12:00:18 +0530

    Create Admin.js

---
+++ Back/models/Admin.js
@@ -0,0 +1,32 @@
+const { DataTypes } = require('sequelize');
+const sequelize = require('../config/database');
+const bcrypt = require('bcryptjs');
+
+const Admin = sequelize.define('Admin', {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+  email: {
+    type: DataTypes.STRING,
+    allowNull: false,
+    unique: true
+  },
+  password: {
+    type: DataTypes.STRING,
+    allowNull: false
+  }
+});
+
+// Hash the password before saving
+Admin.beforeCreate(async (admin) => {
+  const salt = await bcrypt.genSalt(10);
+  admin.password = await bcrypt.hash(admin.password, salt);
+});
+
+module.exports = Admin;
