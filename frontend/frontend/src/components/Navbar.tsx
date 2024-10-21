c15b5fa2cadde478fa4c0d4b1eaa8b66f7085760
Boshitha Gunarathna <it21319792@my.sliit.lk>
2024-10-21 12:02:01 +0530

    Create Navbar.tsx

---
+++ frontend/frontend/src/components/Navbar.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Navbar: React.FC = () => {
+  return (
+    <nav className="bg-blue-500 p-4">
+      <div className="container mx-auto">
+        <h1 className="text-white text-2xl">Employee Management</h1>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
